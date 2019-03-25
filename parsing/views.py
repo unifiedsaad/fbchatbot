fb3635d470abeeabd8f169340166ee707cb41f96
unifiedsaad <saad.m@whyunified.com>
2019-03-25 16:15:24 +0500

    changer version

--- parsing/views.py
+++ parsing/views.py
@@ -32,10 +32,10 @@ def post_facebook_message(fbid, recevied_message):
     if not joke_text:
         joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
 
-    
+
     joke_text = 'Yo  Saad..! ' + joke_text
 
-    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
+    post_message_url = 'https://graph.facebook.com/v3.2/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
     print(status)
