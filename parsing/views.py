3d67a7d982fe595b688d326803153aef1087a39e
unifiedsaad <saad.m@whyunified.com>
2019-03-25 12:39:00 +0500

    updating code

---
+++ parsing/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from wit import Wit
+
+# Create your views here.
+
+def parse(self):
+    access_token = '2NH6NQG7FNEFPKHEBPKTWLPD4LJFECXX';
+    client = Wit(access_token)
+    resp = client.message('Who is Waseem')
+    print(resp)
+    return HttpResponse(str(resp))
