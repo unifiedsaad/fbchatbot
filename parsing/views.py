d3f6c45ec003eb3c4a08e88fcd47acb6c5939ec8
unifiedsaad <saad.m@whyunified.com>
2019-03-26 12:33:10 +0500

    final update

--- parsing/views.py
+++ parsing/views.py
@@ -32,18 +32,19 @@ def post_facebook_message(fbid, recevied_message):
     if not joke_text:
         joke_text = "I didn't understand! Send 'stupid', 'fat', 'dumb' for a Yo Mama joke!"
 
-
-    joke_text = 'Yo  Saad..! ' + joke_text
+    user_details_url = "https://graph.facebook.com/v3.2/%s" % fbid
+    user_details_params = {'fields': 'first_name,last_name,profile_pic', 'access_token': PAGE_ACCESS_TOKEN}
+    user_details = requests.get(user_details_url, user_details_params).json()
+    joke_text = 'Yo ' + user_details['first_name'] + '..! ' + joke_text
 
     post_message_url = 'https://graph.facebook.com/v3.2/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient": {"id": fbid}, "message": {"text": joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"}, data=response_msg)
-    print(status)
     pprint(status.json())
 
 
 # Create your views here.
-class parse(generic.View):
+class YoMamaBotView(generic.View):
     def get(self, request, *args, **kwargs):
         if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
             return HttpResponse(self.request.GET['hub.challenge'])
@@ -60,6 +61,14 @@ class parse(generic.View):
         incoming_message = json.loads(self.request.body.decode('utf-8'))
         # Facebook recommends going through every entry since they might send
         # multiple messages in a single call during high load
-        print(incoming_message)
-
-        return HttpResponse(str('Hello World'), status=200)
+        for entry in incoming_message['entry']:
+            for message in entry['messaging']:
+                # Check to make sure the received call is a message call
+                # This might be delivery, optin, postback for other events
+                if 'message' in message:
+                    # Print the message to the terminal
+                    pprint(message)
+                    # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
+                    # are sent as attachments and must be handled accordingly.
+                    post_facebook_message(message['sender']['id'], message['message']['text'])
+        return HttpResponse()
