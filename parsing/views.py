d4b87b3b37040f7d1e231c87f4c60330bb0f83a9
unifiedsaad <saad.m@whyunified.com>
2019-06-12 15:39:16 +0500

    yes

--- parsing/views.py
+++ parsing/views.py
@@ -88,7 +88,7 @@ def handleIntents(receivedent):
             return "hey is asking about the merit info"
         elif receivedent['intent'][0]['value'] == "junk":
             return "that was the junk"
-        elif receivedent['greetings'][0]['value'] == "true":
+        elif receivedent['greetings']['value'] == "true":
             return "hey, how can i help you "
     except Exception as e:
         print("not found dude")
